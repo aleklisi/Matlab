95e577ca6ac51f1ce5b408c9cc73c69558863d34
aleklisi <sebaczechu123@gmail.com>
2017-11-08 18:58:06 +0000

    Add files via upload

---
+++ zad4_2.m
@@ -0,0 +1,21 @@
+clearvars;
+close all;
+clc;
+%%
+phobos = imread('phobos.bmp');
+histeq(phobos,256);
+%%
+load histogramZadany;
+figure();
+bar(histogramZadany);
+%%
+ phoboSaDJUST = imadjust(phobos);
+ phoboada = adapthisteq(phobos);
+ 
+ imshow(phoboSaDJUST);
+ figure();
+ imshow(phoboada);
+ %%
+ 
+ 
+ 
